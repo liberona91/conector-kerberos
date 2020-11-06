68b1332848f4d6f1709ac582e3f69667dec0dd5d
Jose Quintanilla <jose.quintanilla@accenture.com>
2020-11-06 11:46:27 -0300

    JQ fix

--- src/main/java/cl/accenture/conectorkerberos/controller/ConectorController.java
+++ src/main/java/cl/accenture/conectorkerberos/controller/ConectorController.java
@@ -1,15 +1,21 @@
 package cl.accenture.conectorkerberos.controller;
+
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
 @RequestMapping("/api")
-public class conector {
+public class ConectorController {
+	
+	final static Logger logger = Logger.getLogger(ConectorController.class);
+	
 	@PostMapping("/prueba")
     public String prueba () {
+		logger.info("prueba - init");
         return "Hola";
     }
 	
